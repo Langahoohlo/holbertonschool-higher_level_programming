d5ba9051a67c2f36a974aae26ee63d9b0e05fab0
langahoohlo <langahoohlo@gmail.com>
2024-05-09 11:13:00 +0200

    task 12 bug fix

--- python-if_else_loops_functions/12-fizzbuzz.py
+++ python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    num = range(0, 101)
+    num = range(1, 101)
     for i in num:
-        if i % 3 == 0:
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz", end=' ')
+            continue
+        elif i % 3 == 0:
             print("Fizz", end=' ')
         elif i % 5 == 0:
             print("Buzz", end=' ')
-        elif i % 3 == 0 and i % 5 == 0:
-            print(f"{i}", end=' ')
         else:
             print(f"{i}", end=' ')
