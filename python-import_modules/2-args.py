960411c2e1682ba7026b82b9b15e1de68a6b1e54
langahoohlo <langahoohlo@gmail.com>
2024-05-10 12:03:11 +0200

    fixes

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 import sys
 
-arguments = sys.argv
-num_of_arg = len(arguments)
+if __name__ == "__main__"
+    arguments = sys.argv
+    num_of_arg = len(arguments)
 
-if num_of_arg > 1:
-    for i in range(1, num_of_arg):
-        print("{}: {}" .format(str(i), arguments[i]))
+    if num_of_arg > 1:
+        for i in range(1, num_of_arg):
+            print("{}: {}" .format(str(i), arguments[i]))
 
-else:
-    print("{} arguments." .format(num_of_arg - 1))
+    else:
+        print("{} arguments." .format(num_of_arg - 1))
