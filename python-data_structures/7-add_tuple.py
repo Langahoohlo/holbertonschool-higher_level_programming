1ff4d66faaff8f0de609c87a0dd7a45edfb9f4ed
langahoohlo <langahoohlo@gmail.com>
2024-05-14 23:11:58 +0200

    fixed indentation

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,10 +5,10 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if not tuple_a and not tuple_b:
         sum1, sum2 = 0, 0
 
-        max_length = max(len(tuple_a), len(tuple_b))
-        padded_tuple_a = tuple_a + (0,) * (max_length - len(tuple_a))
-        padded_tuple_b = tuple_b + (0,) * (max_length - len(tuple_b))
+    max_length = max(len(tuple_a), len(tuple_b))
+    padded_tuple_a = tuple_a + (0,) * (max_length - len(tuple_a))
+    padded_tuple_b = tuple_b + (0,) * (max_length - len(tuple_b))
 
-        total = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
+    total = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
 
-        return total
+    return total
