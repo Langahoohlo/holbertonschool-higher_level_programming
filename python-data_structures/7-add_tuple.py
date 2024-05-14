be1add1ac88e1f45d4bded9cbda85c7f12cc1dd5
langahoohlo <langahoohlo@gmail.com>
2024-05-14 23:14:36 +0200

    tuple fixes

--- python-data_structures/7-add_tuple.py
+++ python-data_structures/7-add_tuple.py
@@ -5,10 +5,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
     if not tuple_a and not tuple_b:
         sum1, sum2 = 0, 0
 
-    max_length = max(len(tuple_a), len(tuple_b))
-    padded_tuple_a = tuple_a + (0,) * (max_length - len(tuple_a))
-    padded_tuple_b = tuple_b + (0,) * (max_length - len(tuple_b))
+    sum1 = tuple_a[0] if tuple_a else 0
+    sum2 = tuple_a[1] if len(tuple_a) > 1 else 0
+    sum1 += tuple_b[0] if tuple_b else 0
+    sum2 += tuple_b[1] if len(tuple_b) > 1 else 0
 
-    total = tuple(a + b for a, b in zip(padded_tuple_a, padded_tuple_b))
+    total = (sum1, sum2)
 
     return total
