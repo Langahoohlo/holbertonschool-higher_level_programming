9073a97f76a25370f52172973babe4b3923032fe
langahoohlo <langahoohlo@gmail.com>
2024-05-14 11:58:11 +0200

    task 6

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,5 +1,22 @@
 #!/usr/bin/python3
 
 def print_matrix_integer(matrix=[[]]):
-    for row in matrix:
-        print("{}" .format(row))
+    length, i, = len(matrix), 0
+
+    if len(matrix) == 0:
+        return 1
+
+    while i < length:
+        row, j = matrix[i], 0
+        l = len(row)
+
+        while j < l:
+            if j == l - 1:
+                print("{:d}" .format(row[j]))
+
+            else:
+                print("{:d}" .format(row[j]), end=" ")
+
+            j += 1
+
+        i += 1
