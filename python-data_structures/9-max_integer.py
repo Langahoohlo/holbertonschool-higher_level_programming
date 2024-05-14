aa2ee3f4ed920fb1f43b52212d0260aeffe59977
langahoohlo <langahoohlo@gmail.com>
2024-05-14 14:19:35 +0200

    max integer

---
+++ python-data_structures/9-max_integer.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    length = len(my_list)
+
+    if length == 0:
+        return None
+
+    tmp = my_list[0]
+
+    for i in my_list:
+        if i > tmp:
+            tmp = i
+    return(tmp)
