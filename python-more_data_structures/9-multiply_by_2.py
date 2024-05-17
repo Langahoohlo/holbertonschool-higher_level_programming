5b657fd84c8cfee6c511994a478f3a04c8f2df3f
langahoohlo <langahoohlo@gmail.com>
2024-05-17 16:11:45 +0200

    task 9

--- python-more_data_structures/9-multiply_by_2.py
+++ python-more_data_structures/9-multiply_by_2.py
@@ -3,6 +3,8 @@
 def multiply_by_2(a_dictionary):
     b_dictionary = {name: age * 2 for name, age in a_dictionary.items()}
 
+    return b_dictionary
+
 print_sorted_dictionary = \
     __import__('6-print_sorted_dictionary').print_sorted_dictionary
 
