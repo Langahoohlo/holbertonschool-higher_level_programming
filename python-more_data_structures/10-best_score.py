8e7c5bb0c50dc20d2b60b2d1202bfd460194f679
langahoohlo <langahoohlo@gmail.com>
2024-05-17 23:09:52 +0200

    task 11 and 12

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
 
 def best_score(a_dictionary):
-    high_score = 
+    if not a_dictionary:
+        return None
+    items = {k: v for k, v in a_dictionary.items() if v is not None}
+    if not items:
+        return None
+    best = max(items, key=items.get)
+    return best
