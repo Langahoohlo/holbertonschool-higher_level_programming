60f763eab64a3d329bb5d67851ecca27e75ea762
langahoohlo <langahoohlo@gmail.com>
2024-05-19 22:11:32 +0200

    task 6

--- python-classes/6-square.py
+++ python-classes/6-square.py
@@ -1,24 +1,24 @@
 #!/usr/bin/python3
 
-"""Define class Square"""
+"""Define a class Square."""
 
 
 class Square:
-    """Represntes square"""
+    """Represent a square."""
 
     def __init__(self, size=0, position=(0, 0)):
+        """Initialize a new square.
+
+        Args:
+            size (int): The size of the new square.
+            position (int, int): The position of the new square.
+        """
         self.size = size
-        self.postion = position
-    """Initialize a new square.
+        self.position = position
 
-    Args:
-    size (int): The size of the new square.
-    position (int, int): Position of the square
-    """
-
-    @porperty
+    @property
     def size(self):
-        """get/set the size of the square """
+        """Get/set the current size of the square."""
         return (self.__size)
 
     @size.setter
@@ -31,24 +31,24 @@ class Square:
 
     @property
     def position(self):
-        """current position of square"""
+        """Get/set the current position of the square."""
         return (self.__position)
 
-    @postion.setter
+    @position.setter
     def position(self, value):
-        if not (isinstance(value, tuple)) or
+        if (not isinstance(value, tuple) or
                 len(value) != 2 or
-                not all(isinstance(num, int) for num in value) 
-                or not all(num >= 0 for num in value)):
+                not all(isinstance(num, int) for num in value) or
+                not all(num >= 0 for num in value)):
             raise TypeError("position must be a tuple of 2 positive integers")
         self.__position = value
 
     def area(self):
-        """Return are of square"""
+        """Return the current area of the square."""
         return (self.__size * self.__size)
 
     def my_print(self):
-        """Print square using # character."""
+        """Print the square with the # character."""
         if self.__size == 0:
             print("")
             return
