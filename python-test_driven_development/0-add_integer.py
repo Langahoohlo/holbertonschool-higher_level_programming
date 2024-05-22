1b034b172e19ccf88e135e616562a5eab95779e9
langahoohlo <langahoohlo@gmail.com>
2024-05-22 11:23:31 +0200

    0-add_integer.py

---
+++ python-test_driven_development/0-add_integer.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+
+"""
+    Function divides all elements of a matrix
+
+    Example:
+       a = 7
+       b = 20
+
+       add_integer(a, b)
+
+       27
+"""
+
+
+def add_integer(a, b=98):
+    """
+        Args:
+            a: first integer
+            b: second integer
+
+        Returns:
+            Sum of the two integers
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    if a is type(float):
+        int(a)
+    if b is type(float):
+        int(b)
+    return int(a + b)
---
+++ python-test_driven_development/README.md
@@ -0,0 +1,1 @@
+intro to test driven app development
