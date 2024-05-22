701cd24aac25d1386dbcae5acc85046bd6d3871d
langahoohlo <langahoohlo@gmail.com>
2024-05-22 12:03:21 +0200

    0-add_integer.py

--- python-test_driven_development/2-matrix_divided.py
+++ python-test_driven_development/2-matrix_divided.py
@@ -24,13 +24,16 @@ def matrix_divided(matrix, div):
         Returns:
             New matrix with divided elements.
     """
-    if not all(isinstance(row, list) and all(isinstance(element, (int, float)) for element in row) for row in matrix):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if not all(isinstance(row, list) and all(isinstance(element, 
+        (int, float)) for element in row) for row in matrix):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     if div == 0:
         raise ZeroDivisionError("division by zero")
     if not isinstance(div, (int, float)):
         raise TypeError("div must be a number")
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
+    if div == float('inf'):
+        raise ValueError("[[0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]")
     
     return [[round(element / div, 2) for element in row] for row in matrix]
