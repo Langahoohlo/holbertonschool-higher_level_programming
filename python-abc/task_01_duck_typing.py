525ef2f3667449665619a1ebdf451c45dd1a3e09
langahoohlo <langahoohlo@gmail.com>
2024-05-26 20:22:55 +0200

    ABC

--- python-abc/task_01_duck_typing.py
+++ python-abc/task_01_duck_typing.py
@@ -14,6 +14,8 @@ class Shape(ABC):
 
 class Circle(Shape):
     def __init__(self, radius):
+        if radius < 0:
+            raise ValueError("Radius cannot be negative")
         self.radius = radius
 
     def area(self):
@@ -36,4 +38,3 @@ class Rectangle(Shape):
 def shape_info(shape):
     print(f"Area: {shape.area()}")
     print(f"Perimeter: {shape.perimeter()}")
-
