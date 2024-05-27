f45edeb63ea9acb8a2f4b6be7cec9e402334d9b9
langahoohlo <langahoohlo@gmail.com>
2024-05-27 12:38:55 +0200

    bug changes

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -6,5 +6,5 @@ import json
 def save_to_json_file(my_obj, filename):
     """Function to write an object to a text file"""
     with open(filename, 'w', encoding='utf-8') as f:
-        obj = json.dumps(my_obj)
+        obj = json.dumps(my_obj, f)
         return f.write(obj)
