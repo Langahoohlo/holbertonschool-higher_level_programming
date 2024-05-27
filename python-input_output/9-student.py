683c139522714097793cd552e7f2894a9ee0d50f
langahoohlo <langahoohlo@gmail.com>
2024-05-27 14:36:51 +0200

    bug changes

--- python-input_output/9-student.py
+++ python-input_output/9-student.py
@@ -6,12 +6,12 @@ class Student:
     """Class for student"""
 
     def __init__(self, first_name, last_name, age):
-    """class to define first_name, last_name and age"""
-    self.first_name = first_name
-    self.last_name = last_name
-    self.age = age
+        """class to define first_name, last_name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
         
     def to_json(self):
-    """Get a dictionary representation of the Student."""
-    return self.__dict__
+        """Get a dictionary representation of the Student."""
+        return self.__dict__
