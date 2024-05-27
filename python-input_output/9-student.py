608e135c2df30da4dff5ea65f249cbfc48b6377a
langahoohlo <langahoohlo@gmail.com>
2024-05-27 14:21:23 +0200

    bug changes

---
+++ python-input_output/9-student.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A module to define the class student"""
+
+
+def __init__(self, first_name, last_name, age):
+    """class to define first_name, last_name and age"""
+    self.first_name = first_name
+    self.last_name = last_name
+    self.age = age
+
+def to_json(self):
+    """Get a dictionary representation of the Student."""
+    return self.__dict__
