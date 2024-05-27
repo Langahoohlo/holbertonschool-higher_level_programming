466390c69804faf6263f6ddbf6fcc357a0552481
langahoohlo <langahoohlo@gmail.com>
2024-05-27 11:22:06 +0200

    reading in python

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -6,6 +6,3 @@ def write_file(filename="", text=""):
     """Function to overwrite file"""
     with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
-
-nb_characters = write_file("my_first_file.txt", "This School is so cool!\n")
-print(nb_characters)
