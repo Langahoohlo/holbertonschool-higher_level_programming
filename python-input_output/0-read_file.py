c8e4f2eb957925659bf8ef02414c9b66b2ae3f9f
langahoohlo <langahoohlo@gmail.com>
2024-05-27 10:16:24 +0200

    reading in python

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
+"""Module to read a file from argument"""
+
 
 def read_file(filename=""):
+    """Function to read file"""
     with open(filename, 'r', encoding="utf-8") as f:
         read_data = f.read()
