2df24b3511f5b65409b2f0492f740c78d6d2a7de
langahoohlo <langahoohlo@gmail.com>
2024-05-27 11:41:05 +0200

    reading in python

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 import json
-
-'''Module to JSONIFY an object'''
+"""Module to JSONIFY an object"""
 
 
 def to_json_string(my_obj):
-    '''Module to jsonify argument my_obj'''
+    """Module to jsonify argument my_obj"""
     return json.dumps(my_obj)
