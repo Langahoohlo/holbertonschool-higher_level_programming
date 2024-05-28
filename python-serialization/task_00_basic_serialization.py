75c9b7850d4f7e7c7a3fff8175eaaacbbc655e85
langahoohlo <langahoohlo@gmail.com>
2024-05-28 12:55:33 +0200

    task 1

---
+++ python-serialization/task_00_basic_serialization.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import pickle
+
+
+def serialize_and_save_to_file(data, filename):
+    with open(filename, mode='wb') as f:
+        pickle.dump(data, f)
+
+def load_and_deserialize(filename):
+    with open(filename, mode="rb") as f:
+        return pickle.load(f)
