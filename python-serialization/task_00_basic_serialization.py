b0f4d8572141c02318813776f908f61aff2a11a7
langahoohlo <langahoohlo@gmail.com>
2024-05-28 14:17:48 +0200

    task 1

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+"""Module to seralize json and deserialize"""
 import pickle
 
 
 def serialize_and_save_to_file(data, filename):
+    """Function to serialize from deictionary"""
     with open(filename, mode='wb') as f:
         pickle.dump(data, f)
 
 
 def load_and_deserialize(filename):
+    """Function to deciaralize json to dixtionary"""
     with open(filename, mode="rb") as f:
         return pickle.load(f)
---
+++ python-serialization/task_01_pickle.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""module to have a custom objectclass"""
+import pickle
+
+
+class CustomObject:
+    def __init__(self, name, age, is_student):
+        self.name = name
+        self.age = age
+        self.is_student = student
+  
