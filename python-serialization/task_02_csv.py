149b20ce87eb3251db7f3665f1e95e9edba22534
langahoohlo <langahoohlo@gmail.com>
2024-05-29 21:08:33 +0200

    task 02

--- python-serialization/task_02_csv.py
+++ python-serialization/task_02_csv.py
@@ -4,16 +4,27 @@ import csv
 import json
 
 
-def convert_csv_to_json(data):
+def convert_csv_to_json(csvdata):
     """function to convert csv to json
+
     Args:
         data: csv file to converted to json
+
     """
-    with open(data, 'r', newline='') as cs_v:
-        csvReader = csv.DictReader(cs_v)
-        info = list(csvReader)
 
-    json_data = json.dumps(info, indent=4)
+    data = {}
+    try:
+        with open(csvdata, 'r', encoding='utf-8') as cs_v:
+            csvReader = csv.DictReader(cs_v)
+            for row in csvReader:
+                data.append(row)
 
-    with open('info.json', 'w') as jsonfile:
-        jsonfile.write(json_data, indent)
+    except FileNotFoundError:
+        return False
+
+    try:
+        with open("data.json", "w", encoding="utf-8") as jsondata:
+            json.dump(data, jsonfile, indent=4)
+    except:
+        return False
+    return True
