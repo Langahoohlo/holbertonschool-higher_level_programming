0748c2497def3c7918b90cc839bcb19ed3106725
langahoohlo <langahoohlo@gmail.com>
2024-05-29 21:20:04 +0200

    task 00

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -4,12 +4,27 @@ import pickle
 
 
 def serialize_and_save_to_file(data, filename):
-    """Function to serialize from deictionary"""
+    """
+    Function to serialize from dictionary
+
+    Args:
+        data: python object
+        filename: name of file to save
+    """
+
     with open(filename, mode='wb') as f:
         pickle.dump(data, f)
 
 
 def load_and_deserialize(filename):
-    """Function to deciaralize json to dixtionary"""
+    """
+    Function to serialize from dictionary
+
+    Args:
+        filename: name of file to save
+    Returns:
+        returns python object
+    """
+
     with open(filename, mode="rb") as f:
         return pickle.load(f)
