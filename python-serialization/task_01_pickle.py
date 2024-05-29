cfe5732544ab0b76cd1925e2fb3fea05b48e3a4f
langahoohlo <langahoohlo@gmail.com>
2024-05-29 20:32:33 +0200

    task 01

--- python-serialization/task_01_pickle.py
+++ python-serialization/task_01_pickle.py
@@ -4,8 +4,33 @@ import pickle
 
 
 class CustomObject:
+    """ customObject class
+    Args:
+        name: string
+        age: integer
+        is_student: boolen
+    """
+
     def __init__(self, name, age, is_student):
+        """function initializes object"""
         self.name = name
         self.age = age
         self.is_student = student
-  
+
+    def display(self):
+        """functin to print"""
+        print(f"Name: {self.name}")
+        print(f"Age: {self.age}")
+        print(f"Is Student: {self.age} ")
+
+    def serialize(self, filename):
+        """function to serialize instance of object"""
+        with open(filename, "wb") as f:
+            pickle.dump(self, f)
+
+    @classmethod
+    def deserialize(cls, filename):
+        """function to return inctance of customObject"""
+        with open(filename, "rb") as f:
+            r = pickle.load(f)
+        return f
