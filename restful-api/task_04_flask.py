3d7395c2d721a0ed4f11d9c5a5a556e550a02985
langahoohlo <langahoohlo@gmail.com>
2024-06-03 16:22:09 +0200

    task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -16,17 +16,17 @@ def home():
 def data():
     if users:
         return jsonify(users.keys())
-    return
+    return 'No users'
 
 @app.route('/status')
 def status():
-    return('OK')
+    return 'OK'
 
 @app.route('/users/<username>')
 def user(username):
-    if usersname in users.keys():
+    if username in users.keys():
         return (users[username])
-    return '{"error": "User not found"}'
+    return '{"error": "User not found"}', 400
 
 
 @app.route('/add_user', methods=['GET', 'POST'])
@@ -36,16 +36,16 @@ def add_user():
         response_data = data
         username = data["username"]
         if not username:
-            return 'username is empty'
+            return 'username is empty', 400
         if username in users.keys():
-            return 'username already exists'
+            return 'username already exists', 400
         del data["username"]
         users[username] = data
         response = {}
         response["message"] = "User added"
         response["user"] = response_data
-        return
+        return jsonify(response), 201
     else:
-        return('failed')
+        return 'failed', 400
 
 if __name__ == "__main__": app.run()
