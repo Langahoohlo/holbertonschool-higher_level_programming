c584befd56708c9c99a0b7adf463886e062c8630
langahoohlo <langahoohlo@gmail.com>
2024-06-03 16:58:54 +0200

    task 4

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,23 +7,28 @@ app = Flask(__name__)
 
 users = {}
 
-@app.route('/')
+@app.route("/")
 def home():
-    return("Welcome to the Flask API!")
+    return "Welcome to the Flask API!"
 
-@app.route('/data')
-def data():
-    if users:
-        return jsonify(list(users.keys()))
-@app.route('/status')
+
+@app.route("/data")
+def get_data():
+    return jsonify(list(users.keys()))
+
+
+@app.route("/status")
 def status():
     return "OK"
 
-@app.route('/users/<username>')
-def user(username):
-    if username in users.keys():
-        return (users[username])
-    return jsonify({"error": "User not found"}), 404
+
+@app.route("/users/<username>")
+def get_user(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 @app.route("/add_user", methods=["POST"])
 def add_user():
