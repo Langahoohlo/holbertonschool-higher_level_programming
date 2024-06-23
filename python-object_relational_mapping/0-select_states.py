67054ccedf4c051426c4fd265726a8652664041c
langahoohlo <langahoohlo@gmail.com>
2024-06-23 20:48:07 +0200

    task 1

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,25 +1,33 @@
 #!/usr/bin/python3
-"""List all states from hbtn_0e_0_usa db"""
+"""
+Script that lists all states from the database hbtn_0e_0_usa.
 
-
+Args:
+username: name of the mysql user.
+password: password of the mysql user.
+database: name of the mysql database.
+"""
 if __name__ == "__main__":
     import MySQLdb
     import sys
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-# Establish connection using MySQLdb
     db = MySQLdb.connect(
         host="localhost",
         port=3306,
-        user=sys.argv[1],
-        passwd=sys.argv[2],
-        database=sys.argv[3]
+        user=username,
+        passwd=password,
+        db=database
     )
 
-# Create cursor object to interact with db by executing SQL queries
-    cur = db.cursor()
-    cur.execute("""SELECT * FROM states ORDER BY id ASC""")
-    # fetchall() retrieves all rows returned from SQL query in execute()
-    rows = cur.fetchall()
-# Returns list of tuples. Each tuple is a row from the result set of the query
-    for row in rows:
-        print("({0}, '{1}')".format(row[0], row[1]))
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
