bb9c4d5efcabd3ed77ee7ceb77ee060d828cdfd8
langahoohlo <langahoohlo@gmail.com>
2024-06-23 20:29:47 +0200

    python object relational mapping

---
+++ SQL_more_queries/python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+This script lists all states with a name starting with 'N'
+from the database hbtn_0e_0_usa
+
+Arguments:
+    username (str): The username for the MySQL database
+    password (str): The password for the MySQL database
+    database (str): The name of the MySQL database
+
+The script connects to a MySQL server running on localhost at port 3306.
+"""
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database
+    )
+
+    cursor = db.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    cursor.close()
+    db.close()
