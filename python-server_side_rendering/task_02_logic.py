b95b9f0fb13f40b70f42afab2338d51d98cf000e
Langa Hoohlo <langahoohlo@gmail.com>
2024-07-16 20:40:26 +0200

    task 03

--- python-server_side_rendering/task_02_logic.py
+++ python-server_side_rendering/task_02_logic.py
@@ -14,5 +14,13 @@ def about():
 def contact():
     return render_template('contact.html')
 
+@app.route('/items')
+def items():
+    with open('items.json') as f:
+        data = json.load(f)
+    items = data.get("items", [])
+
+    return render_template('items.html', items=items)
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
